3af2fd7d4ec29e12c18cd24946c79360ae884d0f
xubing <xubing314@163.com>
2017-11-20 18:10:08 +0800

    22

--- python_study/com/bing/first.py
+++ python_study/com/bing/first.py
@@ -48,5 +48,5 @@ if __name__ == '__main__':
     myobj = myclass(age = 18,name= 'bing')##根据名字匹配，而不是顺序
     print myobj.age;
     myobj.get_age()
-    myobj.testException(12)  #测试异常  
+    myobj.testException(12)   #测试异常  
     
